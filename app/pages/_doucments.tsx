4cb150b96527c16aa48765f4a821c9d2733b2183
Gromit <ssi02014@naver.com>
2022-05-26 19:37:33 +0900

    init: next, typescript, eslint, styled-components

---
+++ app/pages/_doucments.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+class MyDocument implements Document {
+  styles: any;
+  context: any;
+  setState: any;
+  forceUpdate: any;
+  props: any;
+  state: any;
+  refs: any;
+
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ app/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styled from "styled-components";
+
+const Home: NextPage = () => {
+  return <Box></Box>;
+};
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: red;
+`;
+export default Home;
